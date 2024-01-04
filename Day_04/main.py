b44835856f20c66e270278377c4ef58ec9e6859d
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-01-04 22:19:14 +0530

    day 4 complete

---
+++ Day_04/main.py
@@ -0,0 +1,23 @@
+# Randomisation and Lists
+
+# import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# random_float = random.random()
+# print(random_float)
+
+
+names = ["Mohit", "Brock", "Blazorus"]
+print(names[0])
+print(names[-1])
+
+names[1] = "Brock Lesnar"
+names.append("Goku")
+names.extend(["Naruto", "Vegeta"])
+print(names)
+
+name = ["Pain", "Itachi"]
+new = [names, names]
+print(new)
