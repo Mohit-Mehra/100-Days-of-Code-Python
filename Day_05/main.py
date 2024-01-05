6765993dddf08902331088102028b2b940a45046
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-01-05 21:18:47 +0530

    Day 5 Completed

---
+++ Day_05/main.py
@@ -0,0 +1,15 @@
+# Loops
+
+# For Loop
+fruits = ["Apple", "Banana", "Mango"]
+for fruit in fruits:
+    print(fruit + " Pie")
+
+
+for number in range(1, 11, 3):
+    print(number)
+
+total = 0
+for n in range(1, 101):
+    total += n
+print(total)
