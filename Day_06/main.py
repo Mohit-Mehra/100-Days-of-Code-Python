46915916fe34d42ba184096f14d1e9bbfb1ab705
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-01-06 21:41:38 +0530

    Day 6 Completed

---
+++ Day_06/main.py
@@ -0,0 +1,20 @@
+# Functions
+
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+# Defining Function
+def greeting():
+    print("Hello")
+    print("Bye")
+
+
+# Calling the Function
+greeting()
+
+
+# While Loop
+n = 0
+while n <= 10:
+    print(n)
+    n += 1
