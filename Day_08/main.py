56dc6008411eb099c56a05fb1340a89fc8f05b7e
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2024-01-08 21:56:08 +0530

    Day 8 completed

---
+++ Day_08/main.py
@@ -0,0 +1,26 @@
+def greet():
+    print("Hello")
+    print("How do you do?")
+    print("Ins't the weather nice today?")
+
+
+# greet()
+
+
+# Function with input
+
+
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print("How do you do?")
+    print("Ins't the weather nice today?")
+
+
+# greet_with_name("Mohit")
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Mohit","Haryana")
